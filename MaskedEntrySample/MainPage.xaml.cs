112643ba7b52789ce9cff916e90b4947e4124c8e
Sathiyagandhi Karunanidhi <sathiyagandhi.k@syncfusion.com>
2026-08-03 15:52:54 +0530

    Added the sample folder

---
+++ MaskedEntrySample/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MaskedEntrySample
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private  void OnValidateClicked(object sender, EventArgs e)
+        {
+            if (phoneNumberEntry.HasError)
+            {
+                DisplayAlertAsync("Alert ", "Enter a Valid credentials", "OK");
+                return;
+            }
+            DisplayAlertAsync("Alert ", "Submitted Successfully", "OK");
+            
+        }
+        
+    }
+    
+}
